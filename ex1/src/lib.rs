b02e12a49f8c1460551aaa8448d2a85d66633ec3
Otto <ottorottier@gmail.com>
2022-12-01 09:13:29 +0100

    Day 1

--- ex1/src/lib.rs
+++ ex1/src/lib.rs
@@ -1,5 +1,32 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn sum_largest(input: &str) -> u64 {
+    input
+        .split("\n\n")
+        .map(|chunk| {
+            chunk
+                .trim()
+                .split('\n')
+                .map(|l| l.trim().parse::<u64>().unwrap())
+                .sum()
+        })
+        .max()
+        .unwrap()
+}
+
+pub fn sum_largest_three(input: &str) -> u64 {
+    let mut totals = input
+        .split("\n\n")
+        .map(|chunk| {
+            chunk
+                .trim()
+                .split('\n')
+                .map(|l| l.trim().parse::<u64>().unwrap())
+                .sum()
+        })
+        .collect::<Vec<_>>();
+
+    totals.sort();
+    let len = totals.len();
+    totals[len - 3..len].iter().sum()
 }
 
 #[cfg(test)]
@@ -7,8 +34,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_part_one() {
+        let input = include_str!("../test.input");
+        assert_eq!(sum_largest(input), 24000);
+    }
+
+    #[test]
+    fn part_one() {
+        let input = include_str!("../input");
+        assert_eq!(sum_largest(input), 66616);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = include_str!("../test.input");
+        assert_eq!(sum_largest_three(input), 45000);
+    }
+
+    #[test]
+    fn part_two() {
+        let input = include_str!("../input");
+        assert_eq!(sum_largest_three(input), 199172);
     }
 }
