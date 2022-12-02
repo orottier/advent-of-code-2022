6fb1957b4cb92d2d362c5d5443662f20f7e7fbe0
Otto <ottorottier@gmail.com>
2022-12-02 08:56:18 +0100

    Ex 2B in C

--- ex2/c/test.c
+++ ex2/c/test.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// opponent: A = rock, B = paper, C = scissor
+// me: X = rock (1), Y = paper (2), Z = scissor (3)
+// outcome: X = lose, Y = draw, Z = win
+
 unsigned int score_game(char other, char me) {
     unsigned int score = 0;
 
@@ -43,6 +47,40 @@ unsigned int score_game(char other, char me) {
     return score;
 }
 
+unsigned int score_outcome(char other, char outcome) {
+    switch (outcome) {
+        case 'X':
+            switch (other) {
+                case 'A':
+                    return score_game(other, 'Z');
+                case 'B':
+                    return score_game(other, 'X');
+                case 'C':
+                    return score_game(other, 'Y');
+            }
+        case 'Y':
+            switch (other) {
+                case 'A':
+                    return score_game(other, 'X');
+                case 'B':
+                    return score_game(other, 'Y');
+                case 'C':
+                    return score_game(other, 'Z');
+            }
+        case 'Z':
+            switch (other) {
+                case 'A':
+                    return score_game(other, 'Y');
+                case 'B':
+                    return score_game(other, 'Z');
+                case 'C':
+                    return score_game(other, 'X');
+            }
+    }
+
+    return -1;
+}
+
 int main(void)
 {
     FILE * fp;
@@ -50,7 +88,8 @@ int main(void)
     size_t len = 0;
     ssize_t read;
 
-    unsigned int score = 0;
+    unsigned int score_a = 0;
+    unsigned int score_b = 0;
 
     fp = fopen("../input", "r");
     if (fp == NULL) {
@@ -61,7 +100,10 @@ int main(void)
     while ((read = getline(&line, &len, fp)) != -1) {
         char other = line[0];
         char me = line[2];
-        score += score_game(other, me);
+        score_a += score_game(other, me);
+
+        char outcome = me;
+        score_b += score_outcome(other, outcome);
     }
 
     fclose(fp);
@@ -69,7 +111,8 @@ int main(void)
         free(line);
     }
 
-    printf("score: %d\n", score);
+    printf("score a: %d\n", score_a);
+    printf("score b: %d\n", score_b);
 
     exit(EXIT_SUCCESS);
 }
