90368464d0958fe71e7f76c043486b6fea89de2c
Otto <ottorottier@gmail.com>
2022-12-07 19:43:26 +0100

    Ex7 in Rust

--- ex7/rust/src/lib.rs
+++ ex7/rust/src/lib.rs
@@ -1,5 +1,53 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::HashMap;
+
+pub fn traverse(input: &str) -> (u64, u64) {
+    let mut sizes: HashMap<String, u64> = HashMap::new();
+    let mut dir_stack: Vec<String> = vec![];
+    let mut assert_ls = false;
+
+    for line in input.trim().split('\n') {
+        if assert_ls {
+            assert_ls = false;
+            assert_eq!(line, "$ ls");
+            continue;
+        }
+        if line == "$ cd .." {
+            dir_stack.pop().expect("cannot go dir up");
+            continue;
+        }
+        if line.starts_with("$ cd") {
+            assert_ls = true;
+            dir_stack.push(line[5..].to_string());
+            continue;
+        }
+        if line.starts_with("dir ") {
+            continue;
+        }
+        let mut pieces = line.split(' ');
+        let size: u64 = pieces.next().unwrap().parse().unwrap();
+        for i in 1..=dir_stack.len() {
+            let path = dir_stack[..i].join("/");
+            sizes.entry(path).and_modify(|v| *v += size).or_insert(size);
+        }
+    }
+
+    let sum_small = sizes
+        .iter()
+        .filter(|(_k, v)| **v <= 100000)
+        .map(|(_k, v)| *v)
+        .sum();
+
+    let disk_size = 70000000;
+    let need_free = 30000000;
+    let delete_at_least = need_free + sizes["/"] - disk_size;
+    let delete_dir = sizes
+        .into_iter()
+        .filter(|(_k, v)| *v >= delete_at_least)
+        .map(|(_k, v)| v)
+        .min()
+        .unwrap();
+
+    (sum_small, delete_dir)
 }
 
 #[cfg(test)]
@@ -7,8 +55,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_both() {
+        let input = include_str!("../../test.input");
+        assert_eq!(traverse(input), (95437, 24933642));
+    }
+
+    #[test]
+    fn both() {
+        let input = include_str!("../../input");
+        assert_eq!(traverse(input), (1644735, 1300850));
     }
 }
