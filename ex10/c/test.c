2c92abb9b80f55e857776b50de34ffed8a4a71eb
Otto <ottorottier@gmail.com>
2022-12-10 20:16:24 +0100

    Ex 10 B in C, buggy but somehow shows the right answer

--- ex10/c/test.c
+++ ex10/c/test.c
@@ -3,12 +3,27 @@
 #include <string.h>
 #include <stdbool.h>
 
-void add_cycle(unsigned long* cycle, long value, long* result) {
+#define MAX(x,y) (((x) >= (y)) ? (x) : (y))
+#define MIN(x,y) (((x) <= (y)) ? (x) : (y))
+
+void add_cycle(unsigned long* cycle, long value, long* result, char* buffer) {
     *cycle += 1;
+    long cycle_mod = *cycle % 40;
+
+    unsigned long max = MAX(cycle_mod, value);
+    unsigned long min = MIN(cycle_mod, value);
+    if (max - min <= 1) {
+        buffer[cycle_mod] = '#';
+    }
 
     if ((*cycle + 20) % 40 == 0) {
-        printf("cycle = %ld, value = %ld\n", *cycle, value);
+        // printf("cycle = %ld, value = %ld\n", *cycle, value);
         *result += *cycle * value;
+    }
+
+    if (cycle_mod == 0) {
+        printf("-- %s --\n", buffer);
+        memset(buffer, '.', 40);
     }
 }
 
@@ -27,10 +42,14 @@ int main(void)
 
     long value = 1;
     long result = 0;
-    unsigned long cycle = 1;
+    unsigned long cycle = 0;
+
+    char buffer[41];
+    memset(buffer, '.', 40);
+    buffer[40] = 0;
 
     while ((read = getline(&line, &len, fp)) != -1) {
-        add_cycle(&cycle, value, &result);
+        add_cycle(&cycle, value, &result, buffer);
 
         // printf("Retrieved line of length %zu:\n", read);
         if (strcmp(line, "noop\n") != 0) {
@@ -39,7 +58,7 @@ int main(void)
             sscanf(line, "addx %d", &count);
             value += count;
 
-            add_cycle(&cycle, value, &result);
+            add_cycle(&cycle, value, &result, buffer);
         }
 
         // printf("cycle %ld value %ld\n", cycle, value);
